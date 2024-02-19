8c2ffbdc76db8acec07d9f200fd4345e72d2305f
Youtong0826 <jimmy2007826@gmail.com>
2024-02-20 04:39:16 +0800

    round 928 div4

---
+++ div4/round 928/pB.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define pii pair<int,int>
+#define f first
+#define s second
+#define int long long
+#define all(x) x.begin(), x.end()
+#define sz(x) (int) x.size()
+#define Youtong ios::sync_with_stdio(0); cin.tie(0); cout.tie()
+using namespace std;
+
+void solve(){
+    
+}
+
+signed main(){
+    Youtong;
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
