8469cd6834181b9d3cf38f5f6990b6f4742d3bbc
Youtong0826 <jimmy2007826@gmail.com>
2024-08-04 22:30:25 +0800

    926

---
+++ div2/926/pD.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define pii pair<int,int>
+#define f first
+#define s second
+#define int long long
+#define Youtong ios::sync_with_stdio(0); cin.tie(0); cout.tie()
+using namespace std;
+
+// 1 3 2
+//
+
+void solve(){
+    int n, a, b;
+    cin >> n;
+    vector<int> v;
+    v.resize(n);
+    for (int i = 0; i < n-1; i++){
+        cin >> a >> b;
+        v[a] = b;
+    }
+
+}
+
+signed main(){
+    Youtong;
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
